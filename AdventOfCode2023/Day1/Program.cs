890e78562831ac61fdc66a0b75abaf42795be402
Brett Haney <brett.haney@gmail.com>
2023-12-02 18:50:35 -0500

    Day 1 complete - initial

---
+++ AdventOfCode2023/Day1/Program.cs
@@ -0,0 +1,95 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Buffers;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+var _input = await File.ReadAllLinesAsync("input.txt");
+//Console.WriteLine($"Read {_input.Length} lines");
+var _translations = new Dictionary<string, string>
+	{
+		{"one", "1" },
+		{"two", "2" },
+		{"three", "3" },
+		{"four","4" },
+		{"five","5" },
+		{"six","6" },
+		{"seven","7" },
+		{"eight", "8" },
+		{"nine","9" }
+	};
+
+partOne();
+partTwo();
+
+void partOne()
+{
+	Console.WriteLine("PART 1 start");
+	//var validDigits = "0123456789";
+	//SearchValues<char> digitSearchValues = SearchValues.Create(validDigits);
+	char[] validDigits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+	var calibrationValues = new List<int>();
+	foreach (var value in _input)
+	{
+		var firstIdx = value.IndexOfAny(validDigits);
+		var lastIdx = value.LastIndexOfAny(validDigits);
+		if (firstIdx == -1 || lastIdx == -1)
+		{
+			Console.WriteLine($"didn't find what I was looking for in {value}");
+		}
+		var toParse = value.Substring(firstIdx, 1) + value.Substring(lastIdx, 1);
+		//Console.WriteLine(toParse );
+		calibrationValues.Add(int.Parse(toParse));
+	}
+	Console.WriteLine("finished the loop");
+	var sum = calibrationValues.Sum();
+	Console.WriteLine($"PART 1 - Total is {sum}");
+
+	//correct answer is 56042
+}
+
+
+void partTwo()
+{
+	Console.WriteLine("PART 2 start");
+	var calibrationValues = new List<int>();
+	string[] validDigits = { "0", "1", "one", "2", "two", "3", "three", "4", "four", "5", "five", "6", "six", "7", "seven", "8", "eight", "9", "nine" };
+
+	foreach (var value in _input)
+	{
+		var firstIdx = value.Length - 1;
+		var lastIdx = 0;
+		var firstDigit = string.Empty;
+		var lastDigit = string.Empty;
+		foreach (var digit in validDigits)
+		{
+			var pos = value.IndexOf(digit);
+
+			if (pos > -1 && pos <= firstIdx)
+			{
+				firstIdx = pos;
+				firstDigit = digit;
+			}
+
+			pos = value.LastIndexOf(digit);
+			if (pos > -1 && pos >= lastIdx)
+			{
+				lastIdx = pos;
+				lastDigit = digit;
+			}
+		}
+		var calibration = int.Parse(parseDigit(firstDigit) + parseDigit(lastDigit));
+		calibrationValues.Add(calibration);
+	}
+	var total = calibrationValues.Sum();
+	Console.WriteLine($"PART 2 - Total is {total}");
+}
+
+string parseDigit(string input)
+{
+	if (input.Length == 1)
+	{
+		return input;
+	}
+
+	return _translations[input];
+}
