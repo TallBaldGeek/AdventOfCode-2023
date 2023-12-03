63405a4b850445f11bfe7159e38063d827dddcfd
Brett Haney <brett.haney@gmail.com>
2023-12-03 11:50:09 -0500

    Day 03 starting

---
+++ AdventOfCode2023/Day03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Utilities;
+
+namespace Day03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var _input = Utils.ReadAllResourceLines(Assembly.GetExecutingAssembly(), "input.txt");
+            Console.WriteLine($"found {_input.Length} lines");
+
+        }
+    }
+}
