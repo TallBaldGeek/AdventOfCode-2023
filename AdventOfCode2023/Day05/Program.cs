32d9ca344a06b44f651a1be8f5dda9195e4e00c5
Brett Haney <brett.haney@gmail.com>
2023-12-06 21:29:26 -0500

    Borrowed day 5 part two

--- AdventOfCode2023/Day05/Program.cs
+++ AdventOfCode2023/Day05/Program.cs
@@ -12,8 +12,9 @@
 			//Console.WriteLine($"The lowest location number for any initial seed numbers is {logic.PartOne(input)}");
 			//correct answer is 484023871
 
-			Console.WriteLine($"The lowest location number for any initial seed numbers is {logic.PartTwo(input)}");
-			//correct answer is 484023871
+			//Console.WriteLine($"The lowest location number for any initial seed numbers is {logic.PartTwo(input)}");
+			var partTwo = new PartTwo();
+			Console.WriteLine($"The lowest location number for any initial seed numbers is {partTwo.Process()}");
 
 		}
 	}
