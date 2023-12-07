be2a565603582c21100c6c302a406337d705c6a7
Brett Haney <Brett_Haney@Progressive.com>
2023-12-07 15:27:51 -0500

    day06 part1 complete

---
+++ AdventOfCode2023/Day06/Day06Logic.cs
@@ -0,0 +1,77 @@
+﻿using System.Reflection;
+using Utilities;
+
+namespace Day06
+{
+	public class Day06Logic
+	{
+		private const int STARTING_SPEED = 0;
+		private const int ACCELERATION = 1;
+
+		public static string[] GetInput()
+		{
+			return Utils.ReadAllResourceLines(Assembly.GetExecutingAssembly(), "input.txt");
+		}
+
+		public static string[] GetSample()
+		{
+			return Utils.ReadAllResourceLines(Assembly.GetExecutingAssembly(), "sample.txt");
+		}
+
+		public int PartOne(string[] input)
+		{
+			/*
+			Time:      7  15   30
+			Distance:  9  40  200
+			 */
+			var times = input[0]
+				.Split(':', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)[1]
+				.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+				.Select(int.Parse)
+				.ToArray();
+			var distances = input[1]
+				.Split(':', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)[1]
+				.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+				.Select(int.Parse)
+				.ToArray();
+
+			var priorRaceStats = new List<RaceResults>();
+			for (var i = 0; i < times.Length; i++)
+			{
+				priorRaceStats.Add(new RaceResults(times[i], distances[i]));
+			}
+
+			var winningStrategyPossibilities = new List<int>();
+			foreach (var priorRaceStat in priorRaceStats)
+			{
+				winningStrategyPossibilities.Add(priorRaceStat.GetWinningStrategies().Count);
+			}
+
+			return winningStrategyPossibilities.Aggregate((tot, curr) => tot * curr);
+		}
+	}
+
+	record RaceResults(int Duration, int Distance)
+	{
+		public List<WinningStrategy> GetWinningStrategies()
+		{
+			var list = new List<WinningStrategy>();
+			for (int buttonPressDuration = Duration - 1; buttonPressDuration > 0; buttonPressDuration--)
+			{
+				var boatMovementDuration = Duration - buttonPressDuration;
+				var speed = buttonPressDuration;
+				var currentDistance = speed * boatMovementDuration;
+				if (currentDistance > Distance)
+				{
+					list.Add(new WinningStrategy(buttonPressDuration));
+				}
+			}
+			return list;
+		}
+
+	}
+
+	record WinningStrategy(int ButtonPressDuration)
+	{
+	}
+}
---
+++ AdventOfCode2023/Day06/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Day06
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+			var logic = new Day06Logic();
+			var sample = Day06Logic.GetSample();
+			var input = Day06Logic.GetInput();
+
+			Console.WriteLine($"Answer for Part One is {logic.PartOne(input)}");
+		}
+	}
+}
